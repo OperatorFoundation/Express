90a9a61c034ddd1aa72ca180688522d3ad911108
Dr. Brandon Wiley <brandon@operatorfoundation.org>
2022-07-25 14:47:47 -0500

    Initial import

--- Express/ContentView.swift
+++ Express/ContentView.swift
@@ -7,10 +7,14 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
+struct ContentView: View
+{
+    @ObservedObject
+    var environments: Environments
+
+    var body: some View
+    {
+
     }
 }
 
---
+++ Express/Environments.swift
@@ -0,0 +1,13 @@
+//
+//  Environments.swift
+//  Express
+//
+//  Created by Dr. Brandon Wiley on 7/24/22.
+//
+
+import Foundation
+
+public class Environments
+{
+    
+}
