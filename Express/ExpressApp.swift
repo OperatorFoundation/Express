4b9047eb78dc460092bd3ef2c976c118df0afe08
Dr. Brandon Wiley <brandon@operatorfoundation.org>
2022-07-24 21:15:38 -0500

    Initial Commit

---
+++ Express/ExpressApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpressApp.swift
+//  Express
+//
+//  Created by Dr. Brandon Wiley on 7/24/22.
+//
+
+import SwiftUI
+
+@main
+struct ExpressApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
